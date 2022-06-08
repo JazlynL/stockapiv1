438cea883f9929a5003385e45dc0fab257f63e3f
“JazlynL” <lubenjazlyn35@gmail.com>
2022-06-08 18:58:19 -0400

    Refactored Repository for AlphaVantage.

--- src/main/java/com/careerdevs/stockapiv1/repositories/OverviewRepository.java
+++ src/main/java/com/careerdevs/stockapiv1/repositories/OverviewRepository.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OverviewRepository extends CrudRepository<Overview,Long> {
+    // we are able to access the exact values of all these components within the repository.
     public Optional <Overview> findBySymbol(String symbol);
+    public List<Overview> findByAssetType( String asset);
+    public  Optional<Overview> findByNameType(String name);
     public List<Overview> findByExchange (String exchange);
+    public List <Overview> findByCurrency(String currency);
+    public
 }
