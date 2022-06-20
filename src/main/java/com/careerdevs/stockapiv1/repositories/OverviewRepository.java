42265f439922f1e4e80e25d63d4ce4e8ef63a0d2
“JazlynL” <lubenjazlyn35@gmail.com>
2022-06-20 11:32:02 -0400

    working through finishind up backend

--- src/main/java/com/careerdevs/stockapiv1/repositories/OverviewRepository.java
+++ src/main/java/com/careerdevs/stockapiv1/repositories/OverviewRepository.java
@@ -11,24 +11,39 @@ import java.util.Optional;
 public interface OverviewRepository extends JpaRepository<Overview,Long> {
     // we are able to access the exact values of all these components within the repository
      List<Overview> findById(long Id);
+     List<Overview> findByName(String name);
      List<Overview> findBySymbol(String symbol);
      List<Overview> findByExchange (String exchange);
      List<Overview> findByAssetType (String assetType);
-      List<Overview> findByName(String name);
-      List <Overview> findByCurrency(String currency);
+     List <Overview> findByCurrency(String currency);
      List<Overview> findByCountry(String country);
      List <Overview> findBySector(String sector);
+     List<Overview> findByMarketCapGreaterThanEqual(long marketCap);
+     List<Overview> findByMarketCapLessThanEqual (long marketCap);
+     List<Overview> findByYearHighGreaterThanEqual(float yearHigh);
+     List<Overview> findByYearHighLessThanEqual (float yearHigh);
+     List<Overview> findByYearLowGreaterThanEqual(float yearHigh);
+     List<Overview> findByYearLowLessThanEqual (float yearHigh);
+     List <Overview> findByDividendDateGreaterThanEqual(String dividendDate);
+     List<Overview> findByDividendDateLessThanEqual(String dividendDate);
 
 
      List<Overview> deleteById(long Id);
+     List<Overview> deleteByName(String name);
      List<Overview> deleteBySymbol(String symbol);
      List<Overview> deleteByAssetType (String assetType);
-
      List<Overview> deleteByExchange (String exchange);
-     List<Overview> deleteByName(String name);
      List <Overview> deleteByCurrency(String currency);
      List<Overview> deleteByCountry(String country);
      List <Overview> deleteBySector(String sector);
+     List<Overview> deleteByMarketCapGreaterThanEqual(long marketCap);
+     List<Overview> deleteByMarketCapLessThanEqual (long marketCap);
+     List<Overview> deleteByYearHighGreaterThanEqual(float yearHigh);
+     List<Overview> deleteByYearHighLessThanEqual (float yearHigh);
+     List<Overview> deleteByYearLowGreaterThanEqual(float yearHigh);
+     List<Overview> deleteByYearLowLessThanEqual (float yearHigh);
+     List <Overview> deleteByDividendDateGreaterThanEqual(String dividendDate);
+     List<Overview> deleteByDividendDateLessThanEqual(String dividendDate);
 
 
 
